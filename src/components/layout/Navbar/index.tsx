f549f8d38b0a07175060aa8c715bf35a6070cfdc
sujung lee <sujunglee@dejay.co.kr>
2024-07-19 15:48:59 +0900

    fix: 페이지 새로고침 시 li 의 underline 사라지는 버그 수정

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -12,10 +12,10 @@ export default function Navbar() {
   const [selectedNav, setSelectedNav] = useState('/event')
 
   useEffect(() => {
-    const currentPath = NAVIGATION.find((nav) => nav.link === location)
+    const currentPath = NAVIGATION.find((nav) => location.includes(nav.link))
     if (currentPath) {
       setSelectedNav(currentPath.label)
-    }
+    } 
   }, [location])
 
   return (
