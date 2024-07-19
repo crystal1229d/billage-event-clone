ff94b99c0f8591cb59b637373e991a80dcdef5fd
sujung lee <sujunglee@dejay.co.kr>
2024-07-19 14:28:37 +0900

    fix: BottomBanner 반응형 수정

--- src/components/layout/BottomBanner/index.tsx
+++ src/components/layout/BottomBanner/index.tsx
@@ -1,5 +1,6 @@
 import Logo from '@/components/common/Logo';
 import styles from './BottomBanner.module.css';
+import Image from 'next/image';
 
 export default function BottomBanner() {
   return (
@@ -8,6 +9,14 @@ export default function BottomBanner() {
             <div className={`${styles['banner-contents']}`}>
                   <Logo with_service_name={false} /> 
                   <h1>빌리쥐를 지금 시작해 보세요!</h1>
+            </div>
+            <div className={`${styles['btn-box']}`}>
+                <button style={{ marginRight: '10px' }}>
+                  <Image src={`/assets/images/googlePlayBtn.png`} width={162} height={50} alt="GooglePlay" />
+                </button>
+                <button>
+                  <Image src={`/assets/images/iosBtn.png`} width={158} height={50} alt="AppStore" />
+                </button>
             </div>
           </div>
     </div>
