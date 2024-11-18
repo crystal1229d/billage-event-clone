509f44c78e094f8ee1fc6508b54ee5b786f43aab
crystal <crystal1229d@naver.com>
2024-11-18 20:02:39 +0900

    Feat: billageProduct(빌려드려요) 페이지 골조 작업

--- src/app/billageProduct/page.tsx
+++ src/app/billageProduct/page.tsx
@@ -1,3 +1,21 @@
 export default function BillageProductPage() {
-  return <div>BillageProductPage</div>
+  return (
+    <div>
+      {/* Action */}
+      <div>
+        <div>
+          <select />
+          <select />
+          <select />
+        </div>
+        <div>
+          <button />
+          <input type="text" />
+        </div>
+      </div>
+
+      {/* Contents */}
+      <div>등록된 대여물품이 없습니다.</div>
+    </div>
+  )
 }
