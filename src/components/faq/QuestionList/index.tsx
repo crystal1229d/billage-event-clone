a063a1bff456946b172d437707d2827b07a088e0
sujung lee <sujunglee@dejay.co.kr>
2024-11-25 16:05:45 +0900

    Feat: FAQ 페이지 구현 및 Shadcn/ui 설치

--- src/components/faq/QuestionList/index.tsx
+++ src/components/faq/QuestionList/index.tsx
@@ -1,4 +1,11 @@
 import { FAQGuide, QnA } from '@/types'
+import Image from 'next/image'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
 import styles from './QuestionList.module.css'
 
 interface Props {
@@ -7,22 +14,35 @@ interface Props {
 
 export default function QuestionList({ questions }: Props) {
   if (!questions || questions.length === 0) {
-    return (
-      <div className={styles['no-items']}>
-        <span>질문이 없습니다.</span>
-      </div>
-    )
+    return <div className={styles['no-items']}></div>
   }
   return (
-    <div className={styles['qna-wrapper']}>
-      <ul>
+    <div className={styles['qnalist-wrapper']}>
+      <Accordion type="single" collapsible>
         {questions.map((question, index) => (
-          <li key={index} className={styles['question-item']}>
-            <h3>Q. {question.question}</h3>
-            <p dangerouslySetInnerHTML={{ __html: question.answer }} />
-          </li>
+          <AccordionItem
+            value={question.question}
+            key={index}
+            className={styles['qna-wrapper']}
+          >
+            <AccordionTrigger className={styles['question']}>
+              <Image
+                src="/assets/images/question.webp"
+                alt="질문"
+                width={41}
+                height={48}
+                loading="lazy"
+              />
+              <span>{question.question}</span>
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className={styles['answer']}>
+                <p dangerouslySetInnerHTML={{ __html: question.answer }} />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
         ))}
-      </ul>
+      </Accordion>
     </div>
   )
 }
